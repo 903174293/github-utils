34027408a9ee1b229ae830d0d4b2f4d310b7459d
Binary Wang <binarywang@gmail.com>
2016-12-08 14:45:46 +0800

    add test for checkAccoutType

--- src/test/java/com/github/binarywang/utils/GithubUtilsTest.java
+++ src/test/java/com/github/binarywang/utils/GithubUtilsTest.java
@@ -1,21 +1,44 @@
 package com.github.binarywang.utils;
 
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.collections.Lists;
+
 
 /**
  * @author Binary Wang
  */
 public class GithubUtilsTest {
+    private long startTime = System.currentTimeMillis();
 
     @DataProvider
     public Object[][] data() {
-        return new Object[][] { { "abc" }, { "def" } };
+        return new Object[][] { { "sd" } };
+    }
+
+
+    @Test(dataProvider = "data")
+    public void testCheckAccoutType(String id) {
+        if (StringUtils.isBlank(id)) {
+            return;
+        }
+
+        System.out.println(
+            (System.currentTimeMillis() - this.startTime) + " Processing  "
+                + id);
+
+        GithubAccountType accoutType = GithubUtils.checkAccoutType(id);
+
+        if (accoutType == GithubAccountType.NoneExist) {
+            System.err.println(id + " not exist");
+            fail(id + " not exist");
+        }
     }
 
     @Test(dataProvider = "data")
